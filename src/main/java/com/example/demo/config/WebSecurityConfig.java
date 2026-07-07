e317b413db79d1bcef4602e7a8d8693af709a156
barathpandian673-design <barathpandian673@gmail.com>
2026-07-07 15:03:34 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -66,7 +66,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5000", "http://127.0.0.1:5000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5000", "http://127.0.0.1:5000","http://barathpandian673.github.io/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
